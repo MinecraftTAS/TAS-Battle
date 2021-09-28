b7215c32362e05843ee7f663201411c2c7391373
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-09-28 22:56:25 +0200

    Restrict more events

--- TAS-Skywars/src/main/java/de/pfannekuchen/skywars/Events.java
+++ TAS-Skywars/src/main/java/de/pfannekuchen/skywars/Events.java
@@ -6,11 +6,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 
 /**
  * Simple events for the Skywars Plugin
@@ -22,6 +26,9 @@ public class Events implements Listener {
 	@EventHandler public void onPlayerBlockPlace(BlockPlaceEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer()) || e.getBlock().getType() == Material.CHEST); }
 	@EventHandler public void onPlayerDamage(EntityDamageEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getEntity())); }
 	@EventHandler public void onPlayerDrop(PlayerDropItemEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
+	@EventHandler public void onPlayerAchievement(PlayerAdvancementCriterionGrantEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
+	@EventHandler public void onPlayerPickup(EntityPickupItemEvent e) { e.setCancelled(!Game.isRunning); }
+	@EventHandler public void onPlayerConsume(PlayerItemConsumeEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
 	@EventHandler public void onInteractEvent(PlayerInteractEvent e) throws Exception { Game.onInteract(e.getPlayer(), e.getItem(), e.getAction()); }
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent e) {
