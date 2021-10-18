b926f71addc667ea7a5354835d5af9d272dafea2
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-10-18 19:16:38 +0200

    Deny more FFA events

--- TAS-FFA/src/main/java/de/pfannekuchen/ffa/Events.java
+++ TAS-FFA/src/main/java/de/pfannekuchen/ffa/Events.java
@@ -5,11 +5,15 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.destroystokyo.paper.event.player.PlayerAdvancementCriterionGrantEvent;
 
 /**
  * Simple events for the FFA Plugin
@@ -21,6 +25,9 @@ public class Events implements Listener {
 	@EventHandler public void onPlayerBlockPlace(BlockPlaceEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
 	@EventHandler public void onPlayerDamage(EntityDamageEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getEntity())); }
 	@EventHandler public void onPlayerDrop(PlayerDropItemEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
+	@EventHandler public void onPlayerAdvancement(PlayerAdvancementCriterionGrantEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
+	@EventHandler public void onPlayerPickup(EntityPickupItemEvent e) { e.setCancelled(!Game.isRunning); }
+	@EventHandler public void onPlayerConsume(PlayerItemConsumeEvent e) { e.setCancelled(Game.shouldAllowInteraction(e.getPlayer())); }
 	@EventHandler public void onInteractEvent(PlayerInteractEvent e) throws Exception { Game.onInteract(e.getPlayer(), e.getItem(), e.getAction()); }
 	@EventHandler
 	public void onPlayerJoinEvent(PlayerJoinEvent e) {
