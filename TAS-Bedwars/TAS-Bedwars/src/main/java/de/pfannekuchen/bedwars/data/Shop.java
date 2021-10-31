abd6f00eeb46d66de520d9194f67496bb99d6299
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-10-31 14:50:14 +0100

    Shop loot

--- TAS-Bedwars/TAS-Bedwars/src/main/java/de/pfannekuchen/bedwars/data/Shop.java
+++ TAS-Bedwars/TAS-Bedwars/src/main/java/de/pfannekuchen/bedwars/data/Shop.java
@@ -3,10 +3,13 @@ package de.pfannekuchen.bedwars.data;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -16,6 +19,12 @@ import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 import de.pfannekuchen.bedwars.Bedwars;
@@ -46,6 +55,7 @@ public class Shop implements Listener {
 					openUpgradeShop(e.getPlayer());
 				}
 			}
+			e.setCancelled(true);
 		}
 	}
 	
@@ -62,7 +72,133 @@ public class Shop implements Listener {
 	 * @param player Player to open for
 	 */
 	private void openItemShop(Player player) {
-		
+		Inventory inventory = Bukkit.createInventory(null, 54, Component.text("\u00A7bItem Shop"));
+		inventory.addItem(getItemStack(Material.NETHER_STAR, "Quick Buy", 1));
+		inventory.addItem(getItemStack(Material.TERRACOTTA, "Blocks", 1));
+		inventory.addItem(getItemStack(Material.GOLDEN_SWORD, "Weapons", 1));
+		inventory.addItem(getItemStack(Material.CHAINMAIL_BOOTS, "Armor", 1));
+		inventory.addItem(getItemStack(Material.STONE_PICKAXE, "Tools", 1));
+		inventory.addItem(getItemStack(Material.BOW, "Bow", 1));
+		inventory.addItem(getItemStack(Material.BREWING_STAND, "Potions", 1));
+		inventory.addItem(getItemStack(Material.TNT, "Utilities", 1));
+		inventory.addItem(getItemStack(Material.BARRIER, "Close Shop", 1));
+		selectItemPage(player, inventory, 0);
+		player.openInventory(inventory);
+	}
+	
+	/**
+	 * Fills up an Inventory with the shop items
+	 * @param inventory
+	 * @param i
+	 */
+	private void selectItemPage(Player p, Inventory inventory, int i) {
+		for (int j = 0; j < 9; j++) inventory.setItem(j + 9, getItemStack(i == j ? Material.GREEN_STAINED_GLASS_PANE : Material.BLACK_STAINED_GLASS_PANE, "", 1));
+		for (int j = 0; j < (4*9); j++) inventory.setItem(j+18, null);
+		switch (i) {
+			case 0:
+				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "§7Cost: §f4 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "§7Cost: §f10 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+4, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "§7Cost: §26 Emeralds"));
+				inventory.setItem(18+5, getItemStack(Material.BOW, "Bow", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+6, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed Potion V", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+7, getItemStack(Material.TNT, "TNT", 1, "§7Cost: §64 Gold"));
+				inventory.setItem(18+1+9, getItemStack(Material.OAK_PLANKS, "Wood", 16, "§7Cost: §64 Gold"));
+				inventory.setItem(18+2+9, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "§7Cost: §67 Gold"));
+				inventory.setItem(18+3+9, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+4+9, getItemStack(Material.SHEARS, "Shears", 1, "§7Cost: §f20 Iron"));
+				inventory.setItem(18+5+9, getItemStack(Material.ARROW, "Arrow", 8, "§7Cost: §62 Gold"));
+				inventory.setItem(18+6+9, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost Potion V", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+7+9, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "§7Cost: §63 Gold"));
+				return;
+			case 1:
+				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "§7Cost: §f4 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.TERRACOTTA, "Terracotta", 16, "§7Cost: §f8 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.GLASS, "Glass", 4, "§7Cost: §f12 Iron"));
+				inventory.setItem(18+4, getItemStack(Material.END_STONE, "End Stone", 12, "§7Cost: §f24 Iron"));
+				inventory.setItem(18+5, getItemStack(Material.LADDER, "Ladder", 8, "§7Cost: §f3 Iron"));
+				inventory.setItem(18+6, getItemStack(Material.OAK_PLANKS, "Wood", 16, "§7Cost: §64 Gold"));
+				inventory.setItem(18+7, getItemStack(Material.OBSIDIAN, "Obsidian", 4, "§7Cost: §24 Emeralds"));
+				return;
+			case 2:
+				inventory.setItem(18+1, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "§7Cost: §f10 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "§7Cost: §67 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.DIAMOND_SWORD, "Diamond Sword", 1, "§7Cost: §2 Emeralds"));
+				inventory.setItem(18+4, getItemStack(Material.STICK, "Knockback Stick", 1, "§7Cost: §65 Gold", "§7Knockback II"));
+				return;
+			case 3:
+				inventory.setItem(18+1, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "§7Cost: §26 Emeralds"));
+				return;
+			case 4:
+				inventory.setItem(18+1, getItemStack(Material.SHEARS, "Shears", 1, "§7Cost: §f20 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.WOODEN_PICKAXE, "Pickaxe I", 1, "§7Cost: §f10 Iron", "§7Tier: §6I"));
+				inventory.setItem(18+3, getItemStack(Material.WOODEN_AXE, "Axe I", 1, "§7Cost: §f10 Iron", "§7Tier: §6I"));
+				return;
+			case 5:
+				inventory.setItem(18+1, getItemStack(Material.ARROW, "Arrow", 8, "§7Cost: §62 Gold"));
+				inventory.setItem(18+2, getItemStack(Material.BOW, "Bow", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.BOW, "Bow II", 1, "§7Cost: §624 Gold", "§7Power I"));
+				inventory.setItem(18+4, getItemStack(Material.BOW, "Bow III", 1, "§7Cost: §26 Emeralds", "§7Power I", "§7Punch I"));
+				return;
+			case 6:
+				inventory.setItem(18+1, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed V Potion", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+2, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost V Potion", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+3, getItemStack(new PotionEffect(PotionEffectType.INVISIBILITY, 30*20, 0), Color.fromRGB(0x9c9d97), "Invisibility Potion", 1, "§7Cost: §22 Emerald"));
+				return;
+			case 7:
+				inventory.setItem(18+1, getItemStack(Material.GOLDEN_APPLE, "Golden Apple", 1, "§7Cost: §63 Gold"));
+				inventory.setItem(18+2, getItemStack(Material.FIRE_CHARGE, "Fireball", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.TNT, "TNT", 1, "§7Cost: §64 Gold"));
+				inventory.setItem(18+4, getItemStack(Material.ENDER_PEARL, "Ender Pearl", 1, "§7Cost: §24 Emerald"));
+				inventory.setItem(18+5, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "§7Cost: §63 Gold"));
+				return;
+		}
+	}
+
+	/**
+	 * Creates a Potion
+	 * @param mat Material of Item Stack
+	 * @param name Name of Item Stack
+	 * @param amount Item Stack count
+	 * @return Item Stack
+	 */
+	private ItemStack getItemStack(PotionEffect potion, Color color, String name, int amount, String... lore) {
+		ItemStack item = new ItemStack(Material.POTION, amount);
+		PotionMeta meta = ((PotionMeta) item.getItemMeta());
+		meta.addCustomEffect(potion, true);
+		meta.setColor(color);
+		item.setItemMeta(meta);
+		item.editMeta(e -> {
+			e.displayName(Component.text("\u00A7f" + name.replace('§', '\u00A7')));
+			if (lore != null) {
+				List<Component> l = new ArrayList<>();
+				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('§', '\u00A7')));
+				e.lore(l);
+			}
+		});
+		return item;
+	}
+	
+	/**
+	 * Creates an Item Stack with a name, material and count
+	 * @param mat Material of Item Stack
+	 * @param name Name of Item Stack
+	 * @param amount Item Stack count
+	 * @return Item Stack
+	 */
+	private ItemStack getItemStack(Material mat, String name, int amount, String... lore) {
+		ItemStack item = new ItemStack(mat, amount);
+		item.editMeta(e -> {
+			e.displayName(Component.text("\u00A7f" + name.replace('§', '\u00A7')));
+			if (lore != null) {
+				List<Component> l = new ArrayList<>();
+				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('§', '\u00A7')));
+				e.lore(l);
+			}
+		});
+		return item;
 	}
 
 	private static Villager[] upgradeShopLocations;
@@ -131,7 +267,6 @@ public class Shop implements Listener {
 	 */
 	private static Villager summonVillager(Location loc) {
 		Villager villager = (Villager) Bedwars.PRIMARYWORLD.spawnEntity(loc.clone().add(.5, 0, .5), EntityType.VILLAGER);
-		villager.setInvisible(true);
 		villager.setInvulnerable(true);
 		villager.setGravity(false);
 		villager.setAI(false);
