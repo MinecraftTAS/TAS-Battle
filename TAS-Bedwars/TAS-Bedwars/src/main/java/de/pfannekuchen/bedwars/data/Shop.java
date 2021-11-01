7c0bc985cb90648bde080ec06307df2075389c48
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-11-01 13:15:57 +0100

    Dirty Shop Code

--- TAS-Bedwars/TAS-Bedwars/src/main/java/de/pfannekuchen/bedwars/data/Shop.java
+++ TAS-Bedwars/TAS-Bedwars/src/main/java/de/pfannekuchen/bedwars/data/Shop.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -24,6 +25,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
@@ -53,6 +55,9 @@ public class Shop implements Listener {
 	public void onShopInvInteract(InventoryClickEvent e) {
 		Inventory inv = e.getInventory();
 		if (inv == null || e.getClickedInventory() == null) return;
+		if (inv instanceof CraftingInventory && e.getSlot() >= 36 && e.getSlot() <= 39) {
+			e.setCancelled(true);
+		}
 		if (PlainTextComponentSerializer.plainText().serialize(e.getView().title()).contains("Item Shop") && e.getClickedInventory().equals(inv)) {
 			e.setCancelled(true);
 			int slot = e.getSlot();
@@ -103,7 +108,11 @@ public class Shop implements Listener {
 										}
 										if (cost <= 0) break;
 									}
-									e.getWhoClicked().getInventory().addItem(buyItem((Player) e.getWhoClicked(), e.getCurrentItem()));
+									try {
+										e.getWhoClicked().getInventory().addItem(buyItem((Player) e.getWhoClicked(), e.getCurrentItem()));
+									} catch (Exception e3) {
+										
+									}
 									e.getWhoClicked().playSound(Sound.sound(org.bukkit.Sound.BLOCK_NOTE_BLOCK_PLING, Source.BLOCK, 1f, 2f));
 								} else {
 									e.getWhoClicked().playSound(Sound.sound(org.bukkit.Sound.BLOCK_ANVIL_LAND, Source.BLOCK, 1f, 1f));
@@ -116,9 +125,42 @@ public class Shop implements Listener {
 		}
 	}
 	
+	public static ArrayList<Player> chainmailArmor = new ArrayList<>();
+	public static ArrayList<Player> ironArmor = new ArrayList<>();
+	public static ArrayList<Player> diamondArmor = new ArrayList<>();
+	
 	public ItemStack buyItem(Player p, ItemStack i) {
 		ItemStack buying = i.clone();
 		buying.lore(null);
+		String itemname = PlainTextComponentSerializer.plainText().serialize(buying.getItemMeta().displayName());
+		System.out.println(itemname);
+		if (itemname.contains("Pickaxe")) {
+			buying.editMeta(e -> {
+				e.displayName(Component.text(itemname.split(" ")[0]));
+			});
+		} else if (itemname.contains("Axe")) {
+			buying.editMeta(e -> {
+				e.displayName(Component.text(itemname.split(" ")[0]));
+			});
+		} else if (itemname.contains("Chainmail Armor")) {
+			if (chainmailArmor.contains(p)) chainmailArmor.remove(p);
+			if (ironArmor.contains(p)) ironArmor.remove(p);
+			if (diamondArmor.contains(p)) diamondArmor.remove(p);
+			chainmailArmor.add(p);
+			return null;
+		} else if (itemname.contains("Iron Armor")) {
+			if (chainmailArmor.contains(p)) chainmailArmor.remove(p);
+			if (ironArmor.contains(p)) ironArmor.remove(p);
+			if (diamondArmor.contains(p)) diamondArmor.remove(p);
+			ironArmor.add(p);
+			return null;
+		} else if (itemname.contains("Diamond Armor")) {
+			if (chainmailArmor.contains(p)) chainmailArmor.remove(p);
+			if (ironArmor.contains(p)) ironArmor.remove(p);
+			if (diamondArmor.contains(p)) diamondArmor.remove(p);
+			diamondArmor.add(p);
+			return null;
+		}
 		return buying;
 	}
 	
@@ -149,6 +191,33 @@ public class Shop implements Listener {
 	public static void tick() {
 		faceAt(itemShopLocations);
 		faceAt(upgradeShopLocations);
+		checkArmor(Bukkit.getOnlinePlayers());
+	}
+	
+	/**
+	 * @param onlinePlayers
+	 */
+	private static void checkArmor(@NotNull Collection<? extends Player> onlinePlayers) {
+		for (Player p : onlinePlayers) {
+			ItemStack armor = p.getInventory().getLeggings();
+			if (armor == null) {
+				p.getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+				p.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS));
+				continue;
+			}
+			if (armor.getType() != Material.CHAINMAIL_LEGGINGS && chainmailArmor.contains(p)) {
+				p.getInventory().setLeggings(new ItemStack(Material.CHAINMAIL_LEGGINGS));
+				p.getInventory().setBoots(new ItemStack(Material.CHAINMAIL_BOOTS));
+			}
+			if (armor.getType() != Material.IRON_LEGGINGS && ironArmor.contains(p)) {
+				p.getInventory().setLeggings(new ItemStack(Material.IRON_LEGGINGS));
+				p.getInventory().setBoots(new ItemStack(Material.IRON_BOOTS));
+			}
+			if (armor.getType() != Material.DIAMOND_LEGGINGS && diamondArmor.contains(p)) {
+				p.getInventory().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
+				p.getInventory().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+			}
+		}
 	}
 
 	/**
@@ -205,63 +274,63 @@ public class Shop implements Listener {
 		for (int j = 0; j < (4*9); j++) inventory.setItem(j+18, null);
 		switch (i) {
 			case 0:
-				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "ï¿½7Cost: ï¿½f4 Iron"));
-				inventory.setItem(18+2, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "ï¿½7Cost: ï¿½f10 Iron"));
-				inventory.setItem(18+3, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "ï¿½7Cost: ï¿½f40 Iron"));
-				inventory.setItem(18+4, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "ï¿½7Cost: ï¿½26 Emeralds"));
-				inventory.setItem(18+5, getItemStack(Material.BOW, "Bow", 1, "ï¿½7Cost: ï¿½612 Gold"));
-				inventory.setItem(18+6, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed Potion V", 1, "ï¿½7Cost: ï¿½21 Emerald"));
-				inventory.setItem(18+7, getItemStack(Material.TNT, "TNT", 1, "ï¿½7Cost: ï¿½64 Gold"));
-				inventory.setItem(18+1+9, getItemStack(Material.OAK_PLANKS, "Wood", 16, "ï¿½7Cost: ï¿½64 Gold"));
-				inventory.setItem(18+2+9, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "ï¿½7Cost: ï¿½67 Gold"));
-				inventory.setItem(18+3+9, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "ï¿½7Cost: ï¿½612 Gold"));
-				inventory.setItem(18+4+9, getItemStack(Material.SHEARS, "Shears", 1, "ï¿½7Cost: ï¿½f20 Iron"));
-				inventory.setItem(18+5+9, getItemStack(Material.ARROW, "Arrow", 8, "ï¿½7Cost: ï¿½62 Gold"));
-				inventory.setItem(18+6+9, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost Potion V", 1, "ï¿½7Cost: ï¿½21 Emerald"));
-				inventory.setItem(18+7+9, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "ï¿½7Cost: ï¿½63 Gold"));
+				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "§7Cost: §f4 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "§7Cost: §f10 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+4, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "§7Cost: §26 Emeralds"));
+				inventory.setItem(18+5, getItemStack(Material.BOW, "Bow", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+6, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed Potion V", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+7, getItemStack(Material.TNT, "TNT", 1, "§7Cost: §64 Gold"));
+				inventory.setItem(18+1+9, getItemStack(Material.OAK_PLANKS, "Wood", 16, "§7Cost: §64 Gold"));
+				inventory.setItem(18+2+9, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "§7Cost: §67 Gold"));
+				inventory.setItem(18+3+9, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+4+9, getItemStack(Material.SHEARS, "Shears", 1, "§7Cost: §f20 Iron"));
+				inventory.setItem(18+5+9, getItemStack(Material.ARROW, "Arrow", 8, "§7Cost: §62 Gold"));
+				inventory.setItem(18+6+9, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost Potion V", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+7+9, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "§7Cost: §63 Gold"));
 				return;
 			case 1:
-				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "ï¿½7Cost: ï¿½f4 Iron"));
-				inventory.setItem(18+2, getItemStack(Material.TERRACOTTA, "Terracotta", 16, "ï¿½7Cost: ï¿½f8 Iron"));
-				inventory.setItem(18+3, getItemStack(Material.GLASS, "Glass", 4, "ï¿½7Cost: ï¿½f12 Iron"));
-				inventory.setItem(18+4, getItemStack(Material.END_STONE, "End Stone", 12, "ï¿½7Cost: ï¿½f24 Iron"));
-				inventory.setItem(18+5, getItemStack(Material.LADDER, "Ladder", 8, "ï¿½7Cost: ï¿½f3 Iron"));
-				inventory.setItem(18+6, getItemStack(Material.OAK_PLANKS, "Wood", 16, "ï¿½7Cost: ï¿½64 Gold"));
-				inventory.setItem(18+7, getItemStack(Material.OBSIDIAN, "Obsidian", 4, "ï¿½7Cost: ï¿½24 Emeralds"));
+				inventory.setItem(18+1, getItemStack(Material.WHITE_WOOL, "Wool", 16, "§7Cost: §f4 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.TERRACOTTA, "Terracotta", 16, "§7Cost: §f8 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.GLASS, "Glass", 4, "§7Cost: §f12 Iron"));
+				inventory.setItem(18+4, getItemStack(Material.END_STONE, "End Stone", 12, "§7Cost: §f24 Iron"));
+				inventory.setItem(18+5, getItemStack(Material.LADDER, "Ladder", 8, "§7Cost: §f3 Iron"));
+				inventory.setItem(18+6, getItemStack(Material.OAK_PLANKS, "Wood", 16, "§7Cost: §64 Gold"));
+				inventory.setItem(18+7, getItemStack(Material.OBSIDIAN, "Obsidian", 4, "§7Cost: §24 Emeralds"));
 				return;
 			case 2:
-				inventory.setItem(18+1, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "ï¿½7Cost: ï¿½f10 Iron"));
-				inventory.setItem(18+2, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "ï¿½7Cost: ï¿½67 Gold"));
-				inventory.setItem(18+3, getItemStack(Material.DIAMOND_SWORD, "Diamond Sword", 1, "ï¿½7Cost: ï¿½24 Emeralds"));
-				inventory.setItem(18+4, getItemStack(Material.STICK, "Knockback Stick", 1, "ï¿½7Cost: ï¿½65 Gold", "ï¿½7Knockback II"));
+				inventory.setItem(18+1, getItemStack(Material.STONE_SWORD, "Stone Sword", 1, "§7Cost: §f10 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.IRON_SWORD, "Iron Sword", 1, "§7Cost: §67 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.DIAMOND_SWORD, "Diamond Sword", 1, "§7Cost: §24 Emeralds"));
+				inventory.setItem(18+4, getItemStack(Material.STICK, "Knockback Stick", 1, "§7Cost: §65 Gold", "§7Knockback II"));
 				return;
 			case 3:
-				inventory.setItem(18+1, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "ï¿½7Cost: ï¿½f40 Iron"));
-				inventory.setItem(18+2, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "ï¿½7Cost: ï¿½612 Gold"));
-				inventory.setItem(18+3, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "ï¿½7Cost: ï¿½26 Emeralds"));
+				inventory.setItem(18+1, getItemStack(Material.CHAINMAIL_BOOTS, "Chainmail Armor", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.IRON_BOOTS, "Iron Armor", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.DIAMOND_BOOTS, "Diamond Armor", 1, "§7Cost: §26 Emeralds"));
 				return;
 			case 4:
-				inventory.setItem(18+1, getItemStack(Material.SHEARS, "Shears", 1, "ï¿½7Cost: ï¿½f20 Iron"));
-				inventory.setItem(18+2, getItemStack(Material.WOODEN_PICKAXE, "Pickaxe I", 1, "ï¿½7Cost: ï¿½f10 Iron", "ï¿½7Tier: ï¿½6I"));
-				inventory.setItem(18+3, getItemStack(Material.WOODEN_AXE, "Axe I", 1, "ï¿½7Cost: ï¿½f10 Iron", "ï¿½7Tier: ï¿½6I"));
+				inventory.setItem(18+1, getItemStack(Material.SHEARS, "Shears", 1, "§7Cost: §f20 Iron"));
+				inventory.setItem(18+2, getItemStack(Material.WOODEN_PICKAXE, "Pickaxe I", 1, "§7Cost: §f10 Iron", "§7Tier: §6I"));
+				inventory.setItem(18+3, getItemStack(Material.WOODEN_AXE, "Axe I", 1, "§7Cost: §f10 Iron", "§7Tier: §6I"));
 				return;
 			case 5:
-				inventory.setItem(18+1, getItemStack(Material.ARROW, "Arrow", 8, "ï¿½7Cost: ï¿½62 Gold"));
-				inventory.setItem(18+2, getItemStack(Material.BOW, "Bow", 1, "ï¿½7Cost: ï¿½612 Gold"));
-				inventory.setItem(18+3, getItemStack(Material.BOW, "Bow II", 1, "ï¿½7Cost: ï¿½624 Gold", "ï¿½7Power I"));
-				inventory.setItem(18+4, getItemStack(Material.BOW, "Bow III", 1, "ï¿½7Cost: ï¿½26 Emeralds", "ï¿½7Power I", "ï¿½7Punch I"));
+				inventory.setItem(18+1, getItemStack(Material.ARROW, "Arrow", 8, "§7Cost: §62 Gold"));
+				inventory.setItem(18+2, getItemStack(Material.BOW, "Bow", 1, "§7Cost: §612 Gold"));
+				inventory.setItem(18+3, getItemStack(Material.BOW, "Bow II", 1, "§7Cost: §624 Gold", "§7Power I"));
+				inventory.setItem(18+4, getItemStack(Material.BOW, "Bow III", 1, "§7Cost: §26 Emeralds", "§7Power I", "§7Punch I"));
 				return;
 			case 6:
-				inventory.setItem(18+1, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed V Potion", 1, "ï¿½7Cost: ï¿½21 Emerald"));
-				inventory.setItem(18+2, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost V Potion", 1, "ï¿½7Cost: ï¿½21 Emerald"));
-				inventory.setItem(18+3, getItemStack(new PotionEffect(PotionEffectType.INVISIBILITY, 30*20, 0), Color.fromRGB(0x9c9d97), "Invisibility Potion", 1, "ï¿½7Cost: ï¿½22 Emerald"));
+				inventory.setItem(18+1, getItemStack(new PotionEffect(PotionEffectType.SPEED, 45*20, 4), Color.fromRGB(0xFFBF00), "Speed V Potion", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+2, getItemStack(new PotionEffect(PotionEffectType.JUMP, 45*20, 4), Color.fromRGB(0xE6E6FA), "Jump Boost V Potion", 1, "§7Cost: §21 Emerald"));
+				inventory.setItem(18+3, getItemStack(new PotionEffect(PotionEffectType.INVISIBILITY, 30*20, 0), Color.fromRGB(0x9c9d97), "Invisibility Potion", 1, "§7Cost: §22 Emerald"));
 				return;
 			case 7:
-				inventory.setItem(18+1, getItemStack(Material.GOLDEN_APPLE, "Golden Apple", 1, "ï¿½7Cost: ï¿½63 Gold"));
-				inventory.setItem(18+2, getItemStack(Material.FIRE_CHARGE, "Fireball", 1, "ï¿½7Cost: ï¿½f40 Iron"));
-				inventory.setItem(18+3, getItemStack(Material.TNT, "TNT", 1, "ï¿½7Cost: ï¿½64 Gold"));
-				inventory.setItem(18+4, getItemStack(Material.ENDER_PEARL, "Ender Pearl", 1, "ï¿½7Cost: ï¿½24 Emerald"));
-				inventory.setItem(18+5, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "ï¿½7Cost: ï¿½63 Gold"));
+				inventory.setItem(18+1, getItemStack(Material.GOLDEN_APPLE, "Golden Apple", 1, "§7Cost: §63 Gold"));
+				inventory.setItem(18+2, getItemStack(Material.FIRE_CHARGE, "Fireball", 1, "§7Cost: §f40 Iron"));
+				inventory.setItem(18+3, getItemStack(Material.TNT, "TNT", 1, "§7Cost: §64 Gold"));
+				inventory.setItem(18+4, getItemStack(Material.ENDER_PEARL, "Ender Pearl", 1, "§7Cost: §24 Emerald"));
+				inventory.setItem(18+5, getItemStack(Material.WATER_BUCKET, "Water Bucket", 1, "§7Cost: §63 Gold"));
 				return;
 		}
 	}
@@ -280,10 +349,10 @@ public class Shop implements Listener {
 		meta.setColor(color);
 		item.setItemMeta(meta);
 		item.editMeta(e -> {
-			e.displayName(Component.text("\u00A7f" + name.replace('ï¿½', '\u00A7')));
+			e.displayName(Component.text("\u00A7f" + name.replace('§', '\u00A7')));
 			if (lore != null) {
 				List<Component> l = new ArrayList<>();
-				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('ï¿½', '\u00A7')));
+				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('§', '\u00A7')));
 				e.lore(l);
 			}
 		});
@@ -300,10 +369,10 @@ public class Shop implements Listener {
 	private ItemStack getItemStack(Material mat, String name, int amount, String... lore) {
 		ItemStack item = new ItemStack(mat, amount);
 		item.editMeta(e -> {
-			e.displayName(Component.text("\u00A7f" + name.replace('ï¿½', '\u00A7')));
+			e.displayName(Component.text("\u00A7f" + name.replace('§', '\u00A7')));
 			if (lore != null) {
 				List<Component> l = new ArrayList<>();
-				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('ï¿½', '\u00A7')));
+				for (String string : lore) l.add(Component.text("\u00A7f" + string.replace('§', '\u00A7')));
 				e.lore(l);
 			}
 		});
