bbb29b822c031531523437c16b552279f6d2e869
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-12-29 22:25:37 +0100

    Add hub commands

--- TAS-Server/FallbackServer/src/main/java/de/pfannekuchen/fallback/FallbackServer.java
+++ TAS-Server/FallbackServer/src/main/java/de/pfannekuchen/fallback/FallbackServer.java
@@ -1,7 +1,10 @@
 package de.pfannekuchen.fallback;
 
+import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerKickEvent;
+import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -13,11 +16,18 @@ public class FallbackServer extends Plugin implements Listener {
 		if ("lobby".equals(event.getKickedFrom().getName())) return;
 		event.setCancelServer(ProxyServer.getInstance().getServerInfo("lobby"));
 		event.setCancelled(true);
+		
 	}
 	
 	@Override
 	public void onEnable() {
 		getProxy().getPluginManager().registerListener(this, this);
+		getProxy().getPluginManager().registerCommand(this, new Command("spawn", null, "hub", "l", "lobby") {
+			@Override
+			public void execute(CommandSender sender, String[] args) {
+				((ProxiedPlayer) sender).connect(getProxy().getServerInfo("lobby"));
+			}
+		});
 		super.onEnable();
 	}
 	
