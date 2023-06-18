43d8b10d27a6607491769c6fe8da976b1c565e0a
PancakeTAS <pancake@mgnet.work>
2023-06-18 16:25:54 +0200

    Add custom capes

--- Client/src/main/java/com/minecrafttas/tasbattle/system/DataSystem.java
+++ Client/src/main/java/com/minecrafttas/tasbattle/system/DataSystem.java
@@ -6,8 +6,11 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.minecrafttas.tasbattle.TASBattle;
+import com.mojang.blaze3d.platform.NativeImage;
 
 import lombok.Getter;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.resources.ResourceLocation;
 
 /**
@@ -22,7 +25,7 @@ public class DataSystem {
 	private Map<UUID, String> tags;
 	
 	@Getter
-	private Map<UUID, URL> capes;
+	private Map<UUID, ResourceLocation> capes;
 	
 	/**
 	 * Initialize data system
@@ -38,6 +41,8 @@ public class DataSystem {
 	 * @throws Exception Malformed data
 	 */
 	public void parseData(String data) throws Exception {
+		var mc = Minecraft.getInstance();
+		
 		var frags = data.split("\n");
 		for (var tag : frags[0].split("\\,")) {
 			var tagFrags = tag.split("\\:");
@@ -46,7 +51,9 @@ public class DataSystem {
 		
 		for (var cape : frags[1].split("\\,")) {
 			var capeFrags = cape.split("\\:");
-			this.capes.put(UUID.fromString(capeFrags[0]), new URL("https://data.mgnet.work/" + capeFrags[1]));
+			var loc = new ResourceLocation("tasbattle", "cape_" + capeFrags[0]);
+			this.capes.put(UUID.fromString(capeFrags[0]), loc);
+			mc.getTextureManager().register(loc, new DynamicTexture(NativeImage.read(new URL("https://data.mgnet.work/" + capeFrags[1]).openStream())));
 		}
 		
 		TASBattle.LOGGER.info("Parsed " + this.tags.size() + " tags and " + this.capes.size() + " capes.");
