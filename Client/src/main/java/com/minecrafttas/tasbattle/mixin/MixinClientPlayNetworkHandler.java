fc75ba00557862edb4159d6a84034607bf5124b2
PancakeTAS <pancake@mgnet.work>
2023-06-18 14:52:26 +0200

    Receive tags and capes on client

--- Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinClientPlayNetworkHandler.java
+++ Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinClientPlayNetworkHandler.java
@@ -1,11 +1,14 @@
 package com.minecrafttas.tasbattle.mixin;
 
+import java.nio.charset.StandardCharsets;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.minecrafttas.tasbattle.TASBattle;
+import com.minecrafttas.tasbattle.system.DataSystem;
 import com.minecrafttas.tasbattle.system.TickrateChanger;
 
 import net.minecraft.client.multiplayer.ClientPacketListener;
@@ -24,11 +27,20 @@ public class MixinClientPlayNetworkHandler {
 	 */
 	@Inject(method = "handleCustomPayload", at = @At("HEAD"), cancellable = true)
 	public void hookCustomPayloadEvent(ClientboundCustomPayloadPacket packet, CallbackInfo ci) {
-		if (!packet.getIdentifier().equals(TickrateChanger.IDENTIFIER))
-			return;
-			
-		TASBattle.getInstance().getTickrateChanger().changeTickrate(packet.getData().readFloat());
-		ci.cancel();
+		if (packet.getIdentifier().equals(TickrateChanger.IDENTIFIER)) {
+			TASBattle.getInstance().getTickrateChanger().changeTickrate(packet.getData().readFloat());
+			ci.cancel();
+		} else if (packet.getIdentifier().equals(DataSystem.IDENTIFIER)) {
+			try {
+				var data = packet.getData();
+				var bytes = new byte[data.readInt()];
+				data.readBytes(bytes);
+				TASBattle.getInstance().getDataSystem().parseData(new String(bytes, StandardCharsets.UTF_8));
+			} catch (Exception e) {
+				TASBattle.LOGGER.error("Invalid data from server: {}", e);
+			}
+			ci.cancel();
+		}
 	}
 
 }
