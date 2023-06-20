b083c68c8590e54b97af5a69b071f9d24cb3cd0c
PancakeTAS <pancake@mgnet.work>
2023-06-20 20:38:54 +0200

    Add autoupdater

--- Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinMinecraft.java
+++ Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinMinecraft.java
@@ -1,8 +1,17 @@
 package com.minecrafttas.tasbattle.mixin;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import com.minecrafttas.tasbattle.system.DimensionSystem;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.impl.ModContainerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,7 +40,7 @@ import net.minecraft.resources.ResourceLocation;
  */
 @Mixin(Minecraft.class)
 public class MixinMinecraft {
-	
+
 	@Shadow
 	private LocalPlayer player;
 	
@@ -74,4 +83,26 @@ public class MixinMinecraft {
 		else if (s instanceof TitleScreen)
 			TASBattle.getInstance().getTickrateChanger().changeTickrate(20.0f);
 	}
+
+	@Inject(method = "destroy", at = @At(value = "INVOKE", shift = At.Shift.BEFORE, target = "Ljava/lang/System;exit(I)V"))
+	public void onClose(CallbackInfo ci) {
+		var mod = ((ModContainerImpl) FabricLoader.getInstance().getModContainer("tasbattle").get()).getOrigin().getPaths().get(0);
+		if (Files.isDirectory(mod))
+			return;
+
+		try {
+			TASBattle.LOGGER.info("Updating TAS Battle mod...");
+			var data = URI.create("https://data.mgnet.work/tasbattle/client.jar").toURL().openStream().readAllBytes();
+
+			var out = new FileOutputStream(mod.toFile());
+			out.write(data);
+			out.flush();
+			out.close();
+
+			TASBattle.LOGGER.info("TAS Battle mod update successful.");
+		} catch (Exception e) {
+			TASBattle.LOGGER.error("TAS Battle mod update unsuccessful.", e);
+		}
+	}
+
 }
