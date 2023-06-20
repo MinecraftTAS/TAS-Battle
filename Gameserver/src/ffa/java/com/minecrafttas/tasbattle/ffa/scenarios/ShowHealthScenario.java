ebef906618ec54c905e75aeb40f072a6a685baf8
PancakeTAS <pancake@mgnet.work>
2023-06-21 00:41:20 +0200

    Fix exception in show health scenario

--- Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
@@ -2,6 +2,7 @@ package com.minecrafttas.tasbattle.ffa.scenarios;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Criteria;
@@ -23,22 +24,19 @@ public class ShowHealthScenario extends AbstractScenario {
 
 	@Override
 	public void gameStart(List<Player> participants) {
-		// setup scoreboard
-		for (var player : participants) {
-			var scoreboard = player.getScoreboard();
+		var scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+
+		// playerlist health
+		var objective = scoreboard.registerNewObjective("hp_playerlist", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
+		objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 			
-			// playerlist health
-			var objective = scoreboard.registerNewObjective("hp_playerlist", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
-			objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
+		// playerlist health
+		var objective2 = scoreboard.registerNewObjective("hp_nametag", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
+		objective2.setDisplaySlot(DisplaySlot.BELOW_NAME);
 			
-			// playerlist health
-			var objective2 = scoreboard.registerNewObjective("hp_nametag", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
-			objective2.setDisplaySlot(DisplaySlot.BELOW_NAME);
-			
-			for (var participant : participants) {
-				objective.getScore(participant).setScore((int) participant.getHealth());
-				objective2.getScore(participant).setScore((int) participant.getHealth());
-			}
+		for (var participant : participants) {
+			objective.getScore(participant).setScore((int) participant.getHealth());
+			objective2.getScore(participant).setScore((int) participant.getHealth());
 		}
 	}
 
