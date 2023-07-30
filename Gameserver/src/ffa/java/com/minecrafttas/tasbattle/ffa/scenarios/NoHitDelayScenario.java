57a578f86a69334b9cb7906fa07ef985d8be22de
PancakeTAS <pancake@mgnet.work>
2023-07-30 18:22:53 +0200

    Add no hit delay scenario

---
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/NoHitDelayScenario.java
@@ -0,0 +1,21 @@
+package com.minecrafttas.tasbattle.ffa.scenarios;
+
+import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+public class NoHitDelayScenario extends AbstractScenario {
+
+	public NoHitDelayScenario() {
+		super("No hit-delay", new String[] {"Don't confuse this with no hit cooldown,", "this will get rid of invincibility frames!"}, Material.NETHER_STAR);
+	}
+
+	@Override
+	public void gameStart(List<Player> participants) {
+		for (var p : participants)
+			p.setMaximumNoDamageTicks(0);
+	}
+	
+}
