9ace11916ad6225f06862c33fcb70609235d7a2d
PancakeTAS <pancake@mgnet.work>
2023-08-01 18:04:41 +0200

    Show totem count in health scenario

--- Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
@@ -1,17 +1,16 @@
 package com.minecrafttas.tasbattle.ffa.scenarios;
 
-import java.util.List;
-
+import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Criteria;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.RenderType;
 
-import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
-
-import net.kyori.adventure.text.Component;
+import java.util.List;
 
 /**
  * Scenario that shows player health
@@ -19,25 +18,23 @@ import net.kyori.adventure.text.Component;
 public class ShowHealthScenario extends AbstractScenario {
 
 	public ShowHealthScenario() {
-		super("Show Health", new String[] {"Shows the players health on tab"}, Material.RED_DYE);
+		super("Show Health", new String[] {"Shows the players health above the player", "and their totem count on tab"}, Material.RED_DYE);
 	}
 
 	@Override
 	public void gameStart(List<Player> participants) {
 		var scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
 
-		// playerlist health
-		var objective = scoreboard.registerNewObjective("hp_playerlist", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
+		// totems popped
+		var objective = scoreboard.registerNewObjective("totem", Criteria.statistic(Statistic.USE_ITEM, Material.TOTEM_OF_UNDYING), Component.text("Totems"), RenderType.INTEGER);
 		objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 			
-		// playerlist health
-		var objective2 = scoreboard.registerNewObjective("hp_nametag", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
+		// health
+		var objective2 = scoreboard.registerNewObjective("hp", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
 		objective2.setDisplaySlot(DisplaySlot.BELOW_NAME);
 			
-		for (var participant : participants) {
-			objective.getScore(participant).setScore((int) participant.getHealth());
+		for (var participant : participants)
 			objective2.getScore(participant).setScore((int) participant.getHealth());
-		}
 	}
 
 }
