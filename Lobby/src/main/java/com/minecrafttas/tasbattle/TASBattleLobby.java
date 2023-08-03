f48cd7727a4b67d5b291f89ba748b2bdaf2a75f0
PancakeTAS <pancake@mgnet.work>
2023-08-03 23:37:05 +0200

    Add logging to lobby

--- Lobby/src/main/java/com/minecrafttas/tasbattle/TASBattleLobby.java
+++ Lobby/src/main/java/com/minecrafttas/tasbattle/TASBattleLobby.java
@@ -3,6 +3,7 @@ package com.minecrafttas.tasbattle;
 import com.minecrafttas.tasbattle.managers.*;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,9 +26,13 @@ public class TASBattleLobby extends JavaPlugin implements Listener {
 	@Getter
 	private ScoreboardManager scoreboardManager;
 
+	@Getter
+	private LobbyTelemetry lobbyTelemetry;
+
 	/**
 	 * Enable tasbattle lobby mod
 	 */
+	@SneakyThrows
 	@Override
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(this, this);
@@ -37,6 +42,13 @@ public class TASBattleLobby extends JavaPlugin implements Listener {
 		this.entityManager = new EntityManager(this);
 		this.basicRestrictions = new BasicRestrictions(this);
 		this.scoreboardManager = new ScoreboardManager(this);
+		this.lobbyTelemetry = new LobbyTelemetry(this);
+	}
+
+	@SneakyThrows
+	@Override
+	public void onDisable() {
+		this.lobbyTelemetry.onShutdown();
 	}
 
 	@EventHandler
