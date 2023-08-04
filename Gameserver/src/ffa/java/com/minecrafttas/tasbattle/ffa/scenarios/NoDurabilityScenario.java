5ee0760343e8778400075d7c3a2e3a8a30e71b53
PancakeTAS <pancake@mgnet.work>
2023-08-04 19:47:09 +0200

    No durability scenario

---
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/NoDurabilityScenario.java
@@ -0,0 +1,34 @@
+package com.minecrafttas.tasbattle.ffa.scenarios;
+
+import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class NoDurabilityScenario extends AbstractScenario {
+
+	private JavaPlugin plugin;
+
+	public NoDurabilityScenario(JavaPlugin plugin) {
+		super("Disable durability", new String[] {"Items won't lose durability"}, Material.BARRIER);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void gameStart(List<Player> participants) {
+		Bukkit.getPluginManager().registerEvents(this, this.plugin);
+	}
+	
+	@EventHandler
+	public void onItemDamage(PlayerItemDamageEvent e) {
+		e.setCancelled(true);
+	}
+	
+}
