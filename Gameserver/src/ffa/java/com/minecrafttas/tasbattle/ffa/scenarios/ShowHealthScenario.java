fa02a24bb3fa5dde9cb3b163326e5f170e4d5e08
PancakeTAS <pancake@mgnet.work>
2023-08-05 23:56:30 +0200

    Update health scenario

--- Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/ShowHealthScenario.java
@@ -2,10 +2,14 @@ package com.minecrafttas.tasbattle.ffa.scenarios;
 
 import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.scoreboard.Criteria;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.RenderType;
@@ -18,23 +22,25 @@ import java.util.List;
 public class ShowHealthScenario extends AbstractScenario {
 
 	public ShowHealthScenario() {
-		super("Show Health", new String[] {"Shows the players health above the player", "and their totem count on tab"}, Material.RED_DYE);
+		super("Show Health", new String[] {"Shows the players health on tab", "and popped totems in chat"}, Material.RED_DYE);
 	}
 
 	@Override
 	public void gameStart(List<Player> participants) {
 		var scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-
-		// totems popped
-		var objective = scoreboard.registerNewObjective("totem", Criteria.statistic(Statistic.USE_ITEM, Material.TOTEM_OF_UNDYING), Component.text("Totems"), RenderType.INTEGER);
-		objective.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 			
 		// health
 		var objective2 = scoreboard.registerNewObjective("hp", Criteria.HEALTH, Component.text("HP"), RenderType.HEARTS);
-		objective2.setDisplaySlot(DisplaySlot.BELOW_NAME);
+		objective2.setDisplaySlot(DisplaySlot.PLAYER_LIST);
 			
 		for (var participant : participants)
 			objective2.getScore(participant).setScore((int) participant.getHealth());
+	}
+
+	@EventHandler
+	public void onTotemPop(PlayerItemConsumeEvent e) {
+		if (e.getItem().getType() == Material.TOTEM_OF_UNDYING)
+			Bukkit.broadcast(MiniMessage.miniMessage().deserialize("<aqua>»</aqua> <gray><green>" + e.getPlayer().getName() + "</green> popped a totem! <aqua>(" + (e.getPlayer().getInventory().all(Material.TOTEM_OF_UNDYING).size() - 1) + " remaining)</aqua></gray>"));;
 	}
 
 }
