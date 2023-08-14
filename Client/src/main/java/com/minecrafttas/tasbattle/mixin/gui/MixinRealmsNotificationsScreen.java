cb588a136726d8f716fad356df93381173200698
PancakeTAS <pancake@mgnet.work>
2023-08-14 21:44:06 +0200

    Fix a mixin warning ???

--- Client/src/main/java/com/minecrafttas/tasbattle/mixin/gui/MixinRealmsNotificationsScreen.java
+++ Client/src/main/java/com/minecrafttas/tasbattle/mixin/gui/MixinRealmsNotificationsScreen.java
@@ -1,10 +1,9 @@
 package com.minecrafttas.tasbattle.mixin.gui;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.realmsclient.gui.screens.RealmsNotificationsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 
 /**
  * This mixin disables realms notifications rendering
@@ -14,6 +13,8 @@ public class MixinRealmsNotificationsScreen {
 
 	/**
 	 * Disable realms notifications rendering
+	 * @author Pancake
+	 * @reason Disable realms notifications rendering
 	 */
 	@Overwrite public void render(PoseStack poseStack, int i, int j, float f) {}
 	
