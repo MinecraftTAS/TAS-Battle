1787acde39e2c1ee75a10b04a31f4c79304c53de
PancakeTAS <pancake@mgnet.work>
2023-08-18 19:27:50 +0200

    Fix crash

--- Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinMinecraft.java
+++ Client/src/main/java/com/minecrafttas/tasbattle/mixin/MixinMinecraft.java
@@ -8,7 +8,6 @@ import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -23,8 +22,6 @@ import java.nio.file.Files;
  */
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
-
-	@Shadow public abstract Minecraft getInstance();
 
 	/**
 	 * Hook game loop method
