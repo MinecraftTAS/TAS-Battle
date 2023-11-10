d25b0f484421be0c7915b4f1f729dbb928edf097
PancakeTAS <pancake@mgnet.work>
2023-11-10 23:26:06 +0100

    Add no pearl cooldown scenario

---
+++ Gameserver/src/ffa/java/com/minecrafttas/tasbattle/ffa/scenarios/NoPearlCooldownScenario.java
@@ -0,0 +1,33 @@
+package com.minecrafttas.tasbattle.ffa.scenarios;
+
+import com.minecrafttas.tasbattle.ffa.managers.ScenarioManager.AbstractScenario;
+import io.papermc.paper.event.player.PlayerItemCooldownEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class NoPearlCooldownScenario extends AbstractScenario {
+
+	private JavaPlugin plugin;
+
+	public NoPearlCooldownScenario(JavaPlugin plugin) {
+		super("No pearl cooldown", new String[] {"This scenario disables ender pearl cooldown"}, Material.ENDER_PEARL);
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void gameStart(List<Player> participants) {
+		Bukkit.getPluginManager().registerEvents(this, this.plugin);
+	}
+
+	@EventHandler
+	public void onCooldown(PlayerItemCooldownEvent e) {
+		if (e.getType() == Material.ENDER_PEARL)
+			e.setCancelled(true);
+	}
+
+}
